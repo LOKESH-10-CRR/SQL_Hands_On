c78684353baf2dd0d6890d96a8dd72850138dfbc
Uppala Lokesh <66313746+LOKESH-10-CRR@users.noreply.github.com>
2024-09-20 11:27:44 +0530

    Modified SQL Code for Teddy Demo

--- 1757.sql
+++ 1757.sql
@@ -52,4 +52,7 @@
 
 ---solution
 /* Write your T-SQL query statement below */
-select product_id from Products where low_fats='Y' and recyclable='Y'
+select 
+    product_id
+from Products 
+where low_fats='Y' and recyclable='Y'
